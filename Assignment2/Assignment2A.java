9abd84fb208a4e1f5daf667385d63361a5d9c1f8
Henry Darnell <hjdarnel@uark.edu>
2015-09-29 17:43:25 -0500

    Created isPalindrome function

--- Assignment2/Assignment2A.java
+++ Assignment2/Assignment2A.java
@@ -1,13 +1,34 @@
-// Name:
-
+//Name:
 import java.util.*;
 import java.math.*;
 import static java.lang.Math.*;
 
 public class Assignment2A {
-	
-	public String makePalindrome(String input) {
-		// your code here
-	}
-}
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment2A tester = new Assignment2A();
+//		String a = tester.makePalindrome("abb");
+//		System.out.println(a);
+		boolean b = tester.isPalindrome("racecar");
+		System.out.println(b);
+		
+	}
+	
+//	public String makePalindrome(String input) {
+//		
+//	}
+	public boolean isPalindrome(String input) {
+		String temporary = "";
+		
+		for (int k = input.length() -1; k >= 0; k--){
+			temporary += input.charAt(k);
+		}
+		
+		if (temporary.equals(input))
+			return true;
+		return false;
+	}
+
+
+}
