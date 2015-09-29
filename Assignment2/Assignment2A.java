d17cdddb967458f4473a51c635ea119b818f6c28
Henry Darnell <hjdarnel@uark.edu>
2015-09-29 17:11:38 -0500

    Initial commit

---
+++ Assignment2/Assignment2A.java
@@ -0,0 +1,13 @@
+// Name:
+
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+
+public class Assignment2A {
+	
+	public String makePalindrome(String input) {
+		// your code here
+	}
+}
+
---
+++ Assignment2/Assignment2B.java
@@ -0,0 +1,13 @@
+// Name:
+
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+
+public class Assignment2B {
+	
+	public int numPalindrome(String input) {
+		// your code here
+	}
+}
+
