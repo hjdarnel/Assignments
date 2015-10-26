347e0d0200168429f7368900385c48ea4074d81e
Henry Darnell <hjdarnel@uark.edu>
2015-10-26 08:38:45 -0500

    Test cases, pseudo comments

---
+++ Assignment3/Assignment3A.java
@@ -0,0 +1,24 @@
+// Name: Henry Darnell
+// ID: 010646670
+
+import java.util.*;
+
+public class Assignment3A {
+
+    public static void main(String [ ] args){
+        Assignment3A tester = new Assignment3A();
+        System.out.println(tester.numPaint("BBWWW"));
+        System.out.println(tester.numPaint("BWBWB"));
+        System.out.println(tester.numPaint("WWWWBBB"));
+    }
+
+
+
+    public int numPaint(String input) {
+        int num;
+        //find first W
+        //Find B in the remaining substring / ++ when found
+        
+        return num;
+    }
+}
