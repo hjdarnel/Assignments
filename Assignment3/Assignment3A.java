227e824882677475cf449f17aa837b9d229d20e6
Henry Darnell <hjdarnel@uark.edu>
2015-10-27 15:56:21 -0500

    Changed to proper algorithm -- maybe?

--- Assignment3/Assignment3A.java
+++ Assignment3/Assignment3A.java
@@ -7,31 +7,40 @@ public class Assignment3A {
 
     public static void main(String [ ] args){
         Assignment3A tester = new Assignment3A();
-        System.out.println(tester.numPaint("BBWWW"));
-        System.out.println(tester.numPaint("BWBWB"));
-        System.out.println(tester.numPaint("WWWWBBB"));
+        System.out.println(tester.numPaint("BBBWWWBWWWBWWBBBWWBBBWB"));
+        System.out.println(tester.numPaint("BWBBWBBWBBBBBBBWBBWBWBWBBBBBBBBBBBBBBBBBWBBBBBBBBB"));
+        System.out.println(tester.numPaint("WBBWWBWWWWWBWWBBBWBWWBWBBBWWBWBWBBBWBBBBWBWWBWWBWB"));
     }
 
 
 
     public int numPaint(String input) {
-        int num = 0;
-        int index = 0; //index of first W
-        //find first W
-        for(int i = 0; i < input.length(); i++){
-            if(input.charAt(i) == 'W') {
-                index = i;
-                break;
-            }
-        }
+        int numB = 0;
+        int numW = 0;
+
+        int index = input.indexOf('W'); //index of first W
 
         //Find B in the remaining substring / ++ when found
         for(int i = index; i < input.length(); i++){
             if(input.charAt(i) == 'B')
-                num++;
+                numB++;
         }
 
-        return num;
+        //Find W in the remaining substring / ++ when found
+        index = input.lastIndexOf('B');
+        for(int i = index; i >=  0;  i--){
+            if(input.charAt(i) == 'W')
+                numW++;
+        }
+
+
+        System.out.println("NumB " + numB);
+        System.out.println("NumW " + numW);
+
+        if(numB > numW)
+            return numW;
+        else
+            return numB;
     }
 }
 
