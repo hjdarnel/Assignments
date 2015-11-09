22620b39aa94f861b1a124a60a2457cfe4e061af
Henry Darnell <hjdarnel@uark.edu>
2015-11-09 10:30:38 -0600

    Commit starter files

---
+++ Assignment4/Assignment4A.java
@@ -0,0 +1,13 @@
+// Name:
+
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+
+public class Assignment4A {
+	
+	public int minPrice(String[] tiles) {
+		// your code here
+	}
+}
+
