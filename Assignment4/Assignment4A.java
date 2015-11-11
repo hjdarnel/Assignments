dcfe2acd2276cca5fff661c36da79fa9f14b8169
Henry Darnell <hjdarnel@uark.edu>
2015-11-11 17:06:27 -0600

    Added proper dynamic algorithm instead

--- Assignment4/Assignment4A.java
+++ Assignment4/Assignment4A.java
@@ -6,111 +6,72 @@ import java.math.*;
 import static java.lang.Math.*;
 
 public class Assignment4A {
-//testing
-    boolean DEBUG = true;
 
     public static void main(String[] args) {
         Assignment4A tester = new Assignment4A();
 
-        String[] input = {"2 90 90", "90 2 90", "90 90 2"};
+        long startTime = System.currentTimeMillis();
+        String[] input = {"1 2 3", "10 20 30", "100 200 300"};
+        System.out.println("Testing: " + Arrays.toString(input));
         System.out.println("Price: " +tester.minPrice(input));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) / 1000.0 + " seconds");
         System.out.println("--------------------------------");
 
+        startTime = System.currentTimeMillis();
         String[] input2 = {"5 100 100", "100 100 100", "5 100 100"};
+        System.out.println("Testing: " + Arrays.toString(input2));
         System.out.println("Price: " +tester.minPrice(input2));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) / 1000.0 + " seconds");
         System.out.println("--------------------------------");
 
+        startTime = System.currentTimeMillis();
         String[] input3 = {"30 44 87", "53 64 61", "17 93 103"};
+        System.out.println("Testing: " + Arrays.toString(input3));
         System.out.println("Price: " +tester.minPrice(input3));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) / 1000.0 + " seconds");
         System.out.println("--------------------------------");
 
-        String[] input4 = {"2 4 6", "1 1 1", "4 2 6"};
+        startTime = System.currentTimeMillis();
+        String[] input4 = {"2 3 4", "100 100 100", "53 63 73", "59 42 5", "52 52 46", "7 8 4"};
+        System.out.println("Testing: " + Arrays.toString(input4));
         System.out.println("Price: " +tester.minPrice(input4));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) / 1000.0 + " seconds");
         System.out.println("--------------------------------");
-
-
-
     }
 
     public int minPrice(String[] tiles) {
-        //Copy string array into an int array costs[tile][color]
-        int[][] costs = new int[tiles.length][3];
-        int[] paint = new int[tiles.length]; //0 is black, 1 is grey, 2 is white. Side by side cannot be the same number
-        List<Integer> sums = new ArrayList<Integer>();
-        int mostRecentTileColor = 0;
-        int currentColoringCost = 0;
+        //Copy string array into an int array A[tile][color]
+        int[][] A = new int[tiles.length][3];
 
         for(int i = 0; i < tiles.length; i++){
-            costs[i][0] = Integer.parseInt(tiles[i].split(" ")[0]);
-            costs[i][1] = Integer.parseInt(tiles[i].split(" ")[1]);
-            costs[i][2] = Integer.parseInt(tiles[i].split(" ")[2]);
+            A[i][0] = Integer.parseInt(tiles[i].split(" ")[0]);
+            A[i][1] = Integer.parseInt(tiles[i].split(" ")[1]);
+            A[i][2] = Integer.parseInt(tiles[i].split(" ")[2]);
         }
 
-        if(DEBUG) System.out.println(Arrays.deepToString(costs));
+        int[] current = A[0];
+        int[] next;
 
-
-        for(int k = 0; k < tiles.length; k++) { //this is our iterator for starting the coloring
-            for (int i = k; i < tiles.length; i++) {
-                if (i == 0) {
-                    if (costs[0][0] < costs[0][1]) {
-                        paint[0] = 0;
-                        currentColoringCost += costs[0][0];
-                    } else if (costs[0][2] < costs[0][1] && paint[0 - 1] != 2) {
-                        paint[0] = 2;
-                        currentColoringCost += costs[0][2];
-                    } else {
-                        paint[0] = 1;
-                        currentColoringCost += costs[0][1];
-                    }
-                } else {
-                    if (costs[i][0] < costs[i][1] && paint[i - 1] != 0) {
-                        paint[i] = 0;
-                        currentColoringCost += costs[i][0];
-                    } else if (costs[i][2] < costs[i][1] && paint[i - 1] != 2) {
-                        paint[i] = 2;
-                        currentColoringCost += costs[i][2];
-                    } else {
-                        paint[i] = 1;
-                        currentColoringCost += costs[i][1];
-                    }
-                }
-                if (DEBUG) System.out.println("Cost after tile " + i + " is " + currentColoringCost + ".");
-            }
-            for(int j = 0; j < k; j++){
-                if (j == 0) {
-                    if (costs[0][0] < costs[0][1]) {
-                        paint[0] = 0;
-                        currentColoringCost += costs[0][0];
-                    } else if (costs[0][2] < costs[0][1] && paint[0 - 1] != 2) {
-                        paint[0] = 2;
-                        currentColoringCost += costs[0][2];
-                    } else {
-                        paint[0] = 1;
-                        currentColoringCost += costs[0][1];
-                    }
-                } else {
-                    if (costs[j][0] < costs[j][1] && paint[j - 1] != 0) {
-                        paint[j] = 0;
-                        currentColoringCost += costs[j][0];
-                    } else if (costs[j][2] < costs[j][1] && paint[j - 1] != 2) {
-                        paint[j] = 2;
-                        currentColoringCost += costs[j][2];
-                    } else {
-                        paint[j] = 1;
-                        currentColoringCost += costs[j][1];
-                    }
-                }
-                if (DEBUG) System.out.println("Cost after tile " + j + " is " + currentColoringCost + ".");
-
-            }
-            sums.add(currentColoringCost);
-            currentColoringCost = 0;
+        for(int i = 1; i < A.length; i++){
+            next = A[i];
+            current = findMinPermutation(current, next);
         }
-
-
-        return Collections.min(sums);
+        return Math.min(current[2], Math.min(current[0], current[1]));
     }
 
+    private int[] findMinPermutation(int[] first, int[] second){
+        int[] toReturn = new int[3];
+
+        toReturn[0] = Math.min(first[1] + second[0], first[2] + second[0]);
+        toReturn[1] = Math.min(first[0] + second[1], first[2] + second[1]);
+        toReturn[2] = Math.min(first[0] + second[2], first[1] + second[2]);
+
+        return toReturn;
+    }
 
 }
 
