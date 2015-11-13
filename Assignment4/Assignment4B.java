1ceb32c7806af119cd7f7e0b9f239eb4e5dc47bc
Henry Darnell <hjdarnel@uark.edu>
2015-11-13 12:13:18 -0600

    Before optimization

--- Assignment4/Assignment4B.java
+++ Assignment4/Assignment4B.java
@@ -7,58 +7,146 @@ import static java.lang.Math.*;
 
 public class Assignment4B {
 
+    List<Integer> current;
+    List<Integer> next;
+
+
     public static void main(String[] args) {
         Assignment4B tester = new Assignment4B();
 
-        String[] input5 = {"...S", "....", ".B..", "...."};
+        long startTime = System.currentTimeMillis();
+        String[] input5 = {"B..FS",
+                           "...FF",
+                           "....."};
         System.out.println(tester.minTime(input5));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time to run: " + (endTime - startTime) / 1000.0 + " seconds");
 
-//        String[] input1 = {"B..FS",
-//                           "...FF",
-//                           "....."};
-//        System.out.println(tester.minTime(input1));
-//
-//        String[] input2 = {"B",
-//                           "S"};
-//        System.out.println(tester.minTime(input2));
-//
-//        String[] input3 = {"B..S..S",
-//                           ".B...BB",
-//                           ".S..S.."};
-//        System.out.println(tester.minTime(input3));
-//
-//        String[] input4 = {"BBB..FS.S.S",
-//                           "BBB..F.S.S.",
-//                           "BB.........",
-//                           "BB...FSSSSS"};
-//        System.out.println(tester.minTime(input4));
 
+        startTime = System.currentTimeMillis();
+        String[] input1 = {"B", "S"};
+        System.out.println(tester.minTime(input1));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time to run: " + (endTime - startTime) / 1000.0 + " seconds");
+
+        startTime = System.currentTimeMillis();
+        String[] input2 = {"B..S..S",
+                           ".B...BB",
+                           ".S..S.."};
+        System.out.println(tester.minTime(input2));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time to run: " + (endTime - startTime) / 1000.0 + " seconds");
+
+        startTime = System.currentTimeMillis();
+        String[] input3 = {
+                "...FFFF.",
+                "B..FS...",
+                "..FFFFF.",
+                "........"
+        };
+        System.out.println(tester.minTime(input3));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time to run: " + (endTime - startTime) / 1000.0 + " seconds");
+
+        startTime = System.currentTimeMillis();
+        String[] input4 = {
+                "BBB..FS.S.S",
+                "BBB..F.S.S.",
+                "BB.........",
+                "BB...FSSSSS"
+        };
+
+        System.out.println(tester.minTime(input4));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time to run: " + (endTime - startTime) / 1000.0 + " seconds");
+
+    }
+
+    public int factorial(int n){
+        int z = 1;
+        for(int i = 1; i <= n; i++){
+            z *= i;
+        }
+        return z;
+    }
+
+    public static void swap(int[] in, int a, int b){
+        int tempA = in[a];
+        int tempB = in[b];
+        in[b] = tempA;
+        in[a] = tempB;
     }
 
     public int minTime(String[] map) {
-        return search(map, 1, 2, 0, 3);
+        int lineLength = map[0].length();
+        List<Integer> bikes = new ArrayList<>();
+        List<Integer> spaces = new ArrayList<>();
+
+        for(int i = 0; i < map.length; i++) {
+            for (int k = 0; k < map[0].length(); k++) {
+                if(map[i].charAt(k) == 'B')
+                    bikes.add(i * lineLength + k);
+                else if(map[i].charAt(k) == 'S')
+                    spaces.add(i * lineLength + k);
+            }
+        }
+        long startTime = System.currentTimeMillis();
+
+        //This method for finding permutations was taken from https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+        int n = bikes.size();
+        int[][] bikesP = new int[factorial(n)][n];
+
+        for (int i = 0; i < n; i++)
+            bikesP[0][i] = bikes.get(i); // copy initial permutation to row 0
+
+        for (int i = 1; i < bikesP.length; i++) {
+            bikesP[i] = Arrays.copyOf(bikesP[i-1], n);
+            int k, l;
+            for (k = n - 2; bikesP[i][k] >= bikesP[i][k+1]; k--);
+            for (l = n - 1; bikesP[i][k] >= bikesP[i][l]; l--);
+            swap(bikesP[i], k, l);
+
+            for (int j = 1; k+j < n-j; j++)
+                swap(bikesP[i], k+j, n-j);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time for permutation: " + (endTime - startTime) / 1000.0 + " seconds");
+
+        int sum;
+        int minSum = 9999;
+
+        startTime = System.currentTimeMillis();
+
+        for(int i = 0; i < bikesP.length; i++){
+            sum = 0;
+            for (int k = 0; k < n; k++) {
+                sum += search(map, bikesP[i][k] % lineLength, bikesP[i][k] / lineLength, spaces.get(k) % lineLength, spaces.get(k) / lineLength);
+            }
+            if(sum < minSum)
+                minSum = sum;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for search: " + (endTime - startTime) / 1000.0 + " seconds");
+
+        return minSum;
     }
 
     public int search(String[] map, int sourceX, int sourceY, int destX, int destY){
-
-        boolean isFound = false;
         int lineLength = map[0].length();
-        List<Integer> current = new ArrayList<>();
-        List<Integer> next = new ArrayList<>();
+        if(current == null) current = new ArrayList<>();
+            else current.clear();
+        if(next == null) next = new ArrayList<>();
+            else next.clear();
         int aX;
         int aY;
         int counter = 1;
-        boolean[] visited = new boolean[map.length * map[0].length()];
-        visited[sourceY * lineLength + sourceX] = true;
 
+        boolean[] visited = new boolean[map.length * map[0].length() + 1];
+        visited[sourceY * lineLength + sourceX] = true;
 
         current.add(sourceY * lineLength + sourceX);
 
-        System.out.println("Starting source int index: " + current.toString());
-        System.out.println("sourceX: " + sourceX + " sourceY: " + sourceY);
-        System.out.println("LineLength: " + lineLength + "\n");
-
-        while(!isFound) {
+        while(true) {
             for (Integer a : current) {
                 aX = a % lineLength;
                 aY = a / lineLength;
@@ -68,8 +156,7 @@ public class Assignment4B {
                         next.add((aY - 1) * lineLength + aX);
                         visited[(aY - 1) * lineLength + aX] = true;
 
-                    } else if (map[aY - 1].charAt(aX) == 'S') {
-                        isFound = true;
+                    } else if (map[aY - 1].charAt(aX) == 'S' && destX == aX && destY == aY - 1) {
                         return counter;
 
                     }
@@ -79,8 +166,7 @@ public class Assignment4B {
                         next.add((aY * lineLength) + aX - 1);
                         visited[(aY * lineLength) + aX - 1] = true;
 
-                    } else if (map[aY].charAt(aX - 1) == 'S') {
-                        isFound = true;
+                    } else if (map[aY].charAt(aX - 1) == 'S' && destX == aX -1 && destY == aY) {
                         return counter;
                     }
 
@@ -89,8 +175,7 @@ public class Assignment4B {
                         next.add(((aY + 1) * lineLength) + aX);
                         visited[((aY + 1) * lineLength) + aX] = true;
 
-                    } else if (map[aY + 1].charAt(aX) == 'S') {
-                        isFound = true;
+                    } else if (map[aY + 1].charAt(aX) == 'S' && destX == aX && destY == aY + 1) {
                         return counter;
                     }
 
@@ -98,22 +183,21 @@ public class Assignment4B {
                     if (map[aY].charAt(aX + 1) == '.' || map[aY].charAt(aX + 1) == 'B' ) {
                         next.add((aY) * lineLength + aX + 1);
                         visited[(aY) * lineLength + aX + 1] = true;
-                    } else if (map[aY].charAt(aX + 1) == 'S') {
-                        isFound = true;
+                    } else if (map[aY].charAt(aX + 1) == 'S' && destX == aX + 1 && destY == aY) {
                         return counter;
                     }
 
             }
 
-            System.out.println("next " +  next.toString());
+            if(next.isEmpty())
+                return -1;
             counter++;
             current.clear();
             current.addAll(next);
             next.clear();
         }
 
-
-        return counter;
-
     }
 }
+
+
